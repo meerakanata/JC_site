622db2587143b36d79aa2b04f58841a694048915
Meera Shah <meera_kanata@yahoo.ca>
2020-05-05 22:25:09 -0400

    got the carousel working fully

--- jays_site/src/MyCarousel.js
+++ jays_site/src/MyCarousel.js
@@ -24,36 +24,71 @@ export default class MyCarousel extends React.Component {
         }
     }
 
-managePrevSlidePath = () =>{
-    let id = Number(this.props.slideId);
-    if(id === 1){
-        return "/3";
+
+    managePrevSlidePath = () =>{
+        let id = Number(this.props.slideId);
+        if(id === 1){
+            return "/3";
+        }
+        else if (id === 2){
+            return "/1";
+        }
+        else if (id === 3){
+            return "/2";
+        }
     }
-    else if (id === 2){
-        return "/1";
+
+    manageNextSlidePath = () =>{
+        let id = Number(this.props.slideId);
+        if(id === 1){
+            return "/2";
+        }
+        else if (id === 2){
+            return "/3";
+        }
+        else if (id === 3){
+            return "/1";
+        }
     }
-    else if (id === 3){
-        return "/2";
+
+    showActiveSlideUI = (position) => {
+        
+        let id = Number(this.props.slideId);
+        if(id === position){
+            return <div className="filledDot"></div>;
+        }
+        else if(id !== position){
+            return <div className="emptyDot"></div>;
+        }
     }
-}
+
     render() {
         let id = this.props.currentObj.projID
         let previous = this.managePrevSlidePath();
+        let next = this.manageNextSlidePath();
         return (
             <div className="carousel">
                 <div className="carousel__image">
-                    <img src={this.setCurrentSlide(this.props.currentObj.carousel[0].slide)} alt="slide1"/>
+                    <img src={this.setCurrentSlide(this.props.currentObj.carousel[0].slide)} alt="slide"/>
                 </div>
                 <div className="carousel__blurb">
                     <div className="carousel__blurb-top"></div>
                     <div className="carousel__blurb-bottom">
                         <div className="prevSlide">
-                            <Link to={"/work/"+id+previous}><img src={"../../Icon/Icon w Text/Previous.png"} alt="back arrow"/></Link>
+                            <Link to={"/work/"+id+previous}><img src={"../../Icon/Icon w Text/Previous_Dark.png"} alt="back arrow"/></Link>
                         </div>
-                        <div className="slideOneDot"></div>
-                        <div className="slideTwoDot"></div>
-                        <div className="slideThreeDot"></div>
-                        <div className="nextSlide"></div>
+                        <div className="slideOneDot">
+                            {this.showActiveSlideUI(1)}
+                        </div>
+                        <div className="slideTwoDot">
+                            {this.showActiveSlideUI(2)}
+                        </div>
+                        <div className="slideThreeDot">
+                            {this.showActiveSlideUI(3)}
+                        </div>
+                        <div className="nextSlide">
+                            <Link to={"/work/"+id+next}><img src={"../../Icon/Icon w Text/Next_Dark.png"} alt="forward arrow"/></Link>
+                        </div>
                     </div>
                 </div>
             </div>
